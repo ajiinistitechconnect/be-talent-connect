9cdd4b8fc89d5f3e39ca9e0a2b690a3851ec550b
alwinihza <alwinihza@gmail.com>
2023-06-05 23:59:28 +0700

    [FIX] port production

--- config/config.go
+++ config/config.go
@@ -69,13 +69,13 @@ func (c *Config) ReadConfigFile() error {
 		Password: os.Getenv("DBPASS"),
 	}
 
-	if os.Getenv("PORT") != "" {
-		c.DbConfig.Port = os.Getenv("PORT")
-	}
-
 	c.ApiConfig = ApiConfig{
 		ApiHost: os.Getenv("API_HOST"),
 		ApiPort: os.Getenv("API_PORT"),
+	}
+
+	if os.Getenv("PORT") != "" {
+		c.ApiConfig.ApiPort = os.Getenv("PORT")
 	}
 
 	// c.SMTPConfig = SMTPConfig{
