2aa0bb27821c15e96c159585cc8115e0e44149a7
alwinihza <alwinihza@gmail.com>
2023-06-05 23:46:58 +0700

    [FIX] production

--- config/config.go
+++ config/config.go
@@ -96,8 +96,7 @@ func (c *Config) ReadConfigFile() error {
 	}
 	// c.SMTPEmail == "" || c.SMTPHost == "" || c.SMTPPassword == "" || c.SMTPPort == "" || c.SMTPSenderName == ""
 
-	if c.DbConfig.Host == "" || c.DbConfig.Name == "" || c.DbConfig.Password == "" || c.DbConfig.Port == "" || c.DbConfig.User == "" ||
-		c.ApiConfig.ApiPort == "" {
+	if c.DbConfig.Host == "" || c.DbConfig.Name == "" || c.DbConfig.Password == "" || c.DbConfig.Port == "" || c.DbConfig.User == "" {
 		return errors.New("Missing required field")
 	}
 	return nil
