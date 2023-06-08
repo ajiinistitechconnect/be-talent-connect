79cf70c70b43915299c9f0fecdb32f2ef9889691
ajiinisti <ajiinisti@gmail.com>
2023-06-08 21:08:54 +0700

    [ADD] Mentoring Query

--- model/user.go
+++ model/user.go
@@ -4,9 +4,9 @@ type User struct {
 	BaseModel
 	CreatedBy string `gorm:"default:admin" json:"-"`
 	UpdatedBy string `gorm:"default:admin" json:"-"`
-	Email     string `gorm:"unique" json:"-"`
-	Password  string `json:"-"`
-	IsActive  bool   `gorm:"default:false"`
+	Email     string `gorm:"unique" `
+	Password  string
+	IsActive  bool `gorm:"default:false"`
 	FirstName string
 	LastName  string
 	Roles     []Role `gorm:"many2many:users_roles"`
